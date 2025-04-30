241a7fd312a0d45634ac58aa8ec8b9b85db462da
Ayush Yadav <ayushyadav24nov1234@gmail.com>
2025-04-30 12:21:41 +0530

    first commit

---
+++ Q1.js
@@ -0,0 +1,9 @@
+// Write a function that takes the base and height of a triangle and return its area.
+
+function triArea(base, height) {
+  const result = (base * height) / 2;
+  return result;
+}
+
+const area = triArea(7, 4);
+console.log(area);
